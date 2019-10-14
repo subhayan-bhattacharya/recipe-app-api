9946a32f7bbaac160ca7f0decffac4f5a03ac7ab
Subhayan <subhayan.aws@gmail.com>
2019-10-15 00:54:19 +0200

    Adding postgress support to our app

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -41,6 +41,5 @@ def test_create_user_page(client, setup_users):
     """Test that create user page works"""
     _, _ = setup_users
     url = reverse('admin:core_user_add')
-    print(url)
     res = client.get(url)
     assert str(res.status_code) == "200"
